92123242c3fd4f0b35d6bb049b7425ecd4431d9b
Rijan Dahal <reasondahal719@gmail.com>
2026-06-07 23:27:14 +0545

    feat: add main menu with name input and class selection

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include "Character.h"
+#include "Player.h"
+#include "Warrior.h"
+#include "Enemy.h"
+#include <iostream>
+
+std::string getName() {
+    using namespace std;
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    return name;
+}
+
+int chooseClass() {
+    using namespace std;
+    cout << "1. Warrior\n2. Samurai\n3. Druid\n4. Witch\n";
+    cout << "Select Class: ";
+    int c;
+    cin >> c;
+    return c;
+}
 
 int main() {
+    std::cout << "===== Welcome to Exiled =====\n";
+    std::string name = getName();
+    int classChoice = chooseClass();
+    
+    std::cout << "Thou are welcomed, Tarnished " << name << "!\n";
     return 0;
 }
